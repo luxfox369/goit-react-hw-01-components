6bf92b724b646e3469a229113bb8a58abe4f47db
Olena Pestova <nowalena9@gmail.com>
2023-04-24 12:23:15 +0300

    all in one

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,30 @@
 import { Container } from './App.styled';
-//import { Profile } from '../Profile/Profile';
-//import { Statistics } from '../Statistics/Statistics';
-//import { FriendList } from '../Friends/FriendList';
+import { Profile } from '../Profile/Profile';
+import user from '../../user.json';
+import { Statistics } from '../Statistics/Statistics';
+import stats from '../../data.json';
+import { FriendList } from '../Friends/FriendList';
+import friends from '../../friends.json';
 import { TransactionHistory } from '../Transactions/TransactionHistory';
-//import user from '../../user.json';
-//import stats from '../../data.json';
-//import friends from '../../friends.json';
 import transactions from '../../transactions.json';
 
+//all task in one
+export const App = () => {
+  return (
+    <Container>
+      <Profile
+        username={username}
+        avatar={avatar}
+        tag={tag}
+        location={location}
+      />
+      <Statistics title="Upload stats" data={stats} />
+      <FriendList data={friends} />
+      <TransactionHistory data={transactions} />
+    </Container>
+  );
+};
+//end all task
 //task №1
 // export const App = () => {
 //   const { username, avatar, tag, location } = user;
@@ -48,12 +65,13 @@ import transactions from '../../transactions.json';
 
 //end Task3
 //Task4
-export const App = () => {
-  return (
-    <Container>
-      <TransactionHistory data={transactions} />
-    </Container>
-  );
-};
+// export const App = () => {
+//   return (
+//     <Container>
+
+//       <TransactionHistory data={transactions} />
+//     </Container>
+//   );
+// };
 
 //end Task4
